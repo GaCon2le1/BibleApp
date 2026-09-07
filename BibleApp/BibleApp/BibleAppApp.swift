7e3707b7f91b0bfdff61aea07762821393642aca
GaCon2le1 <107600657+GaCon2le1@users.noreply.github.com>
2026-09-07 11:14:13 +0700

    update

---
+++ BibleApp/BibleApp/BibleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppApp.swift
+//  BibleApp
+//
+//  Created by Gacon on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BibleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
